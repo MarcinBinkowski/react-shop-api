aeb82e57946b96a78eec66f2c59509ed26154370
Marcin <binq661@gmail.com>
2025-05-03 15:42:10 +0200

    test

---
+++ WebApplication1/Controllers/UsersController.cs
@@ -0,0 +1,96 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public UsersController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        return Ok(await _context.Users.ToListAsync());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateUser(User user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser(int id, User updateDto)
+    {
+        if (id != updateDto.Id)
+        {
+            return BadRequest();
+        }
+
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        user.Name = updateDto.Name;
+        user.Email = updateDto.Email;
+        user.Password = updateDto.Password;
+        user.Address = updateDto.Address;
+        user.BirthDate = updateDto.BirthDate;
+        user.JoinedDate = updateDto.JoinedDate;
+        user.IsAdmin = updateDto.IsAdmin;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!await _context.Users.AnyAsync(e => e.Id == id))
+            {
+                return NotFound();
+            }
+            throw;
+        }
+
+        return Ok(user);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
