f80e80218160d5330a57cace9d93b803ad70e7b4
Marcin <binq661@gmail.com>
2025-05-03 17:13:32 +0200

    test

---
+++ WebApplication1/Models/Payment.cs
@@ -0,0 +1,29 @@
+namespace WebApplication1.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Payment
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string PaymentMethod { get; set; } = string.Empty;
+
+    [Required]
+    public decimal Amount { get; set; }
+
+    [Required]
+    public DateTime PaymentDate { get; set; } = DateTime.UtcNow;
+
+    [StringLength(200)]
+    public string? TransactionId { get; set; }
+
+    [StringLength(500)]
+    public string? Notes { get; set; }
+
+    [Required]
+    public int OrderId { get; set; }
+}
